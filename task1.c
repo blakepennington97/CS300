55b01674fc653b5a40185384b7631971734b8085
Blake Pennington <blakepennington97@gmail.com>
2020-09-16 19:03:22 -0500

    task1 complete

---
+++ task1.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int collatz_algo(int n) {
+    if (n%2 == 0) {
+        n = n / 2;
+    }
+    else {
+        n = n * 3 + 1;
+    }
+    return n;
+}
+
+void input_check(int n) {
+    if (n <= 0) {
+        printf("Please provide an integer greater than zero.\n");
+        exit(0);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int starting_num = atoi(argv[1]);
+    int n = starting_num;
+    input_check(starting_num);
+
+    pid_t pid;
+    /* fork a child process */
+    pid = fork();
+
+    if (pid < 0) { /* error occurred */
+        fprintf(stderr, "Fork Failed");
+        return 1;
+    }
+    else if (pid == 0) { /* child process */
+        while (n != 1) {
+            printf("%d, ", n);
+            n = collatz_algo(n);
+        }
+        printf("%d\n", n);
+    }
+    else { /* parent process */
+        /* parent will wait for the child to complete */
+        wait(NULL);
+    }
+    return 0;
+}
+
