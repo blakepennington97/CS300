5df6ebbb183f5cda00979b1546bd41bedd86b153
Blake Pennington <blakepennington97@gmail.com>
2020-10-30 14:00:20 -0500

    manager1 reading in logical addresses and formatting correctly

--- project3/manager1.c
+++ project3/manager1.c
@@ -9,3 +9,37 @@ int page_number_and_offset[2][65536];
 int page_table[256];
 int TLB[2][16];
 int physical_memory[256][256];
+
+
+
+
+void producePageNumber(int logical_address) {
+    printf("Translating logical address: %d\n", logical_address);
+    int page_number = ((logical_address & 0xFFFF) >> 8);
+    int offset = logical_address & 0xFF;
+    printf("page#: %d offset: %d\n", page_number, offset);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Incorrect usage, please format such as: ./manager1.c addresses.txt\n");
+        exit(0);
+    }
+
+    // open addresses.txt
+    FILE *fp = fopen(argv[1], "r");
+    if (fp == NULL) {
+        fprintf(stderr, "Error opening %s\n", argv[1]);
+    }
+
+    char address[12]; //used to contain a single address
+    int logical_address; //used to contain the int version of address
+    while (fgets(address, 12, fp) != NULL) {
+        logical_address = atoi(address);
+        producePageNumber(logical_address);
+    }
+
+
+
+    return 0;
+}
--- project3/manager2.c
+++ project3/manager2.c
@@ -9,3 +9,14 @@ int page_number_and_offset[2][65536];
 int page_table[256];
 int TLB[2][16];
 int physical_memory[256][256];
+
+
+void getPageNumber(int logical_address) {
+
+}
+
+int main() {
+
+
+    return 0;
+}
